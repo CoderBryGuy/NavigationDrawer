88da618bd5f7bef21f47843d446d0dfe53f5d928
Bryan <bryan.dov.bergman@gmail.com>
2022-08-11 22:53:40 +0300

    Nav Drawer done

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,9 +42,46 @@ public class MainActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                return false;
+                selectDrawerItem(item);
+                return true;
             }
         });
+    }
+
+    private void selectDrawerItem(MenuItem item) {
+
+        Fragment fragment = null;
+        Class fragmentClass;
+
+        switch(item.getItemId()){
+            case R.id.nav_first_fragment:
+                fragmentClass = FirstFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = SecondFragment.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = ThirdFragment.class;
+                break;
+            default:
+                fragmentClass = FirstFragment.class;
+        }
+
+        try{
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        }
+
+        FragmentManager fragmentManger = getSupportFragmentManager();
+        fragmentManger.beginTransaction().replace(R.id.flContent, fragment).commit();
+        item.setChecked(true);
+        setTitle(item.getTitle());
+
+        mDrawerLayout.closeDrawers();
+
     }
 
     @Override
